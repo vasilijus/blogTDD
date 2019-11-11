80c7cd757a4f01ca5b79edac565c5cd552ac9c96
Sergej <sergej@computerassistance.co.uk>
2019-11-11 18:05:40 +0000

    fin

--- tests/Feature/SubmitLinksTest.php
+++ tests/Feature/SubmitLinksTest.php
@@ -23,13 +23,13 @@ class SubmitLinksTest extends TestCase
         $response = $this->post('/submit',[
             'title'         => 'ExampleTitle',
             'url'           => 'http://example.com',
-            'description'   => 'Example description.',
+            'description'   => 'ExampleDescription',
         ]);
         
         $this->assertDatabaseHas('links',[
             'title'         => 'ExampleTitle',
         ]);
-
+        // $response->dumpHeaders();
         // Redirect at the end of creation of link
         $response
             ->assertStatus(302)
@@ -39,27 +39,89 @@ class SubmitLinksTest extends TestCase
         $this->get('/')->assertSee('ExampleTitle');
     }
 
-    /** @Test */
+    /** @test */
     function link_is_not_created_if_validation_fails()
     {
-        
+        $response = $this->post('/submit');
+
+        $response->assertSessionHasErrors(['title', 'url', 'description']);
     }
 
-    /** @Test */
-    function link_is_not_created_with_invalid_url()
+    /** @test */
+    function link_is_not_created_with_an_invalid_url()
     {
+        // $this->withoutExceptionHandling();
 
+        $cases = ['//invalid-url.com', '/invalid-url', 'foo.com'];
+
+        foreach ($cases as $case) {
+            try {
+                $response = $this->post('/submit', [
+                    'title' => 'Example Title',
+                    'url' => $case,
+                    'description' => 'Example description',
+                ]);
+            } catch (ValidationException $e) {
+                $this->assertEquals(
+                    'The url format is invalid.',
+                    $e->validator->errors()->first('url')
+                );
+                continue;
+            }
+
+            $this->fail("The URL $case passed validation when it should have failed.");
+        }
     }
 
-    /** @Test */
+    /** @test */
     function max_length_fails_when_too_long()
     {
+        // $this->withoutExceptionHandling();
 
+        $title = str_repeat('a', 256);
+        $description = str_repeat('a', 256);
+        $url = 'http://';
+        $url .= str_repeat('a', 256 - strlen($url));
+    
+        try {
+            $this->post('/submit', compact('title', 'url', 'description'));
+        } catch(ValidationException $e) {
+            $this->assertEquals(
+                'The title may not be greater than 255 characters.',
+                $e->validator->errors()->first('title')
+            );
+    
+            $this->assertEquals(
+                'The url may not be greater than 255 characters.',
+                $e->validator->errors()->first('url')
+            );
+    
+            $this->assertEquals(
+                'The description may not be greater than 255 characters.',
+                $e->validator->errors()->first('description')
+            );
+    
+            return;
+        }
+    
+        $this->fail('Max length should trigger a ValidationException');
     }
-    /** @Test */
+    
+    /** @test */
     function max_length_fails_when_under_max()
     {
+        $url = 'http://';
+        $url.= str_repeat('a', 255 - strlen($url) );
 
+        $data = [
+            'title'         => str_repeat('a',255),
+            'url'           =>  $url,
+            'description'   => str_repeat('a', 255)
+        ];
+
+        $this->post('/submit', $data);
+
+        $this->assertDatabaseHas('links', $data);
     }
 
 }
