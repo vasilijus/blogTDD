bf4678d48d72b8f92ca0982639fb6c8e6e50c46f
Sergej <sergej@computerassistance.co.uk>
2019-11-11 16:34:03 +0000

    prepare tests for form submision!

--- tests/Feature/SubmitLinksTest.php
+++ tests/Feature/SubmitLinksTest.php
@@ -20,32 +20,32 @@ class SubmitLinksTest extends TestCase
         $response->assertStatus(200);
     }
 
-    /** @Test */
+    /** @Test - Verify that valid links get saved in the database */
     function guest_can_submit_a_new_link()
     {
 
     }
 
-    /** @Test */
+    /** @Test - When validation fails, links are not in the database */
     function link_is_not_created_if_validation_fails()
     {
 
     }
-    /** @Test */
+    /** @Test - Invalid URLs are not allowed */
     function link_is_not_created_with_invalid_url()
     {
 
     }
 
-    /** @Test */
+    /** @Test - Validation should fail when the fields are longer than the max:255 validation rule */
     function max_length_fails_when_too_long()
     {
 
     }
-    /** @Test */
+    /** @Test - Validation should succeed when the fields are long enough according to max:255 */
     function max_length_fails_when_under_max()
     {
-        
+
     }
 
 }
